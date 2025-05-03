5bf1b237352cbca16d40cafd98ac157a3b21cb77
Dolani <42421068+Dolani25@users.noreply.github.com>
2025-05-03 16:21:17 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the settings module for your Django project
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Scope.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scope.settings')
 
 # Get the WSGI application
 application = get_wsgi_application()
